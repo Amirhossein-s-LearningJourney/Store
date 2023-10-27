751dd0cac20cbe7effcf2dbe503ba0bd92c92836
amirhossein Edadi <edadiamirhossein@gmail.com>
2023-10-27 12:15:38 +0330

    Smtp4dev set up

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,22 @@
+from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
 from django.shortcuts import render
+from templated_mail.mail import BaseEmailMessage
 
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    try:
+        # sending regular mail
+        # send_mail("subject", "message", "info@store.com", ["jack@store.com"])
+        # sending mail to admin
+        # mail_admins("subj", "message", html_message="message233")
+        # attaching files
+        # mess = EmailMessage("sub", "message", "from@store.com", ["john@store.com"])
+        # mess.attach_file("playground/static/images/dog.jpg")
+        # mess.send()
+        message = BaseEmailMessage(
+            template_name="emails/hello.html", context={"name": "Amir"}
+        )
+        message.send(["john@store.com"])
+    except BadHeaderError:
+        pass
+    return render(request, "hello.html", {"name": "Mosh"})
--- storefront/settings.py
+++ storefront/settings.py
@@ -171,3 +171,13 @@ DJOSER = {
 }
 
 SIMPLE_JWT = {"AUTH_HEADER_TYPES": ("JWT",), "ACCESS_TOKEN_LIFETIME": timedelta(days=1)}
+
+# SMTP configuration
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "localhost"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+EMAIL_PORT = 2525
+DEFAULT_FROM_EMAIL = "from@store.com"
+
+ADMINS = [("Amir", "admin@store.com")]
