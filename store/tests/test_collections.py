6e1dd3a976e63fd910cb82be68501e731607c1ba
amirhossein Edadi <edadiamirhossein@gmail.com>
2023-10-31 09:36:07 +0330

    another test implemented for retrive collection

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,6 +1,8 @@
+from store.models import Collection
 from django.contrib.auth.models import User
 from rest_framework import status
 import pytest
+from model_bakery import baker
 
 
 @pytest.fixture
@@ -38,3 +40,23 @@ class TestCreateCollection:
         response = creat_collection({"title": "a"})
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data["id"] > 0
+
+
+@pytest.mark.django_db
+class TestRetrieveCollection:
+    def test_if_collection_exsists_returns_200(self, api_client):
+        # Arrange
+        # This is against the Rule of Testing behaviours rather than the Implementation but we have no other choice
+        # we can also use api_client to send a post request first instead of the line below but that approach has a lot of bugs
+        # Collection.objects.create(title="a")
+        # a better approach so that we do not have to create all of the fields is to use model bakery like this :
+        collection = baker.make(Collection)
+
+        response = api_client.get(f"/store/collections/{collection.id}/")
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {
+            "id": collection.id,
+            "title": collection.title,
+            "products_count": 0,
+        }
