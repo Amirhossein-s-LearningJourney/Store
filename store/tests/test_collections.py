e9ec6f55da1e370aecbb879c7d6333b3d1a23591
amirhossein Edadi <edadiamirhossein@gmail.com>
2023-10-31 10:11:45 +0330

    furthur tests for product implemented

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,8 +1,9 @@
-from store.models import Collection
+from store.models import Collection, Product
 from django.contrib.auth.models import User
 from rest_framework import status
 import pytest
 from model_bakery import baker
+from django.test import TestCase
 
 
 @pytest.fixture
@@ -13,6 +14,14 @@ def creat_collection(api_client):
         return api_client.post("/store/collections/", collection)
 
     return do_create_collection
+
+
+@pytest.fixture
+def create_product(api_client):
+    def do_create_product(product):
+        return api_client.post("/store/products/", product)
+
+    return do_create_product
 
 
 @pytest.mark.django_db
@@ -60,3 +69,53 @@ class TestRetrieveCollection:
             "title": collection.title,
             "products_count": 0,
         }
+
+
+@pytest.mark.django_db
+class TestCreateProduct:
+    def test_if_user_is_anonymous_returns_401(self, create_product):
+        response = create_product({"title": "Test Product"})
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self, authenticate, create_product):
+        authenticate()
+        response = create_product({"title": "Test Product"})
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_data_is_invalid_returns_400(self, authenticate, create_product):
+        authenticate(is_staff=True)
+        response = create_product({"title": ""})
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert "title" in response.data
+
+    # def test_if_data_is_valid_returns_201(self, authenticate, create_product):
+    #     # Authenticate as a staff member
+    #     authenticate(is_staff=True)
+
+    #     # Create a valid product data
+    #     data = {
+    #         "title": "Test Product",
+    #         "slug": "test-product",
+    #         "unit_price": 20,
+    #         "inventory": 10,
+    #         "collection": baker.make(Collection),
+    #     }
+
+    #     # Create the product using the create_product fixture
+    #     response = create_product(product=data)
+
+    #     # Assert that the response status code is 201
+    #     assert response.status_code == status.HTTP_201_CREATE
+
+
+@pytest.mark.django_db
+class TestRetrieveProduct:
+    def test_if_product_exists_returns_200(self, api_client):
+        # Arrange
+        product = baker.make(Product)
+
+        response = api_client.get(f"/store/products/{product.id}/")
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["id"] == product.id
+        assert response.data["title"] == product.title
